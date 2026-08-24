6620924c8d58b9ba94f6ca34d0036d35f88ab252
Qingsong Hu <kingsanhu@gmail.com>
2026-08-25 00:22:14 +0800

    Fix bug
    Generate bin and hex files not in the form of `.elf.bin` and
    `.elf.hex`

--- cmake/stm32-helpers.cmake
+++ cmake/stm32-helpers.cmake
@@ -1,9 +1,12 @@
 function(stm32_add_hex_bin_targets TARGET_NAME)
+    set(HEX_FILE "$<TARGET_FILE_DIR:${TARGET_NAME}>/$<TARGET_FILE_BASE_NAME:${TARGET_NAME}>.hex")
+    set(BIN_FILE "$<TARGET_FILE_DIR:${TARGET_NAME}>/$<TARGET_FILE_BASE_NAME:${TARGET_NAME}>.bin")
+
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
-        COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${TARGET_NAME}> ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.hex
-        COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${TARGET_NAME}> ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.bin
+        COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${TARGET_NAME}> ${HEX_FILE}
+        COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${TARGET_NAME}> ${BIN_FILE}
         COMMAND ${CMAKE_SIZE} --format=berkeley $<TARGET_FILE:${TARGET_NAME}>
-        COMMENT "Generating ${TARGET_NAME}.hex and ${TARGET_NAME}.bin and printed memory usage:"
+        COMMENT "Generating ${HEX_FILE} and ${BIN_FILE} and printed memory usage:"
     )
 endfunction()
 
