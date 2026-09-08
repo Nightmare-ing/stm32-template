7e1a6723ac681e4ae6a4099e6764e43a99cde53e
Qingsong Hu <kingsanhu@gmail.com>
2026-09-08 15:01:51 +0800

    Refactor deps.cmake
    Collapse drivers.cmake and dependencies.cmake into a single file,
    according to the recommended project structure for CMake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,22 +1,27 @@
 include(FetchContent)
 
+# ======================================================================
+# CMSIS Dependencies from both ARM and STMicroelectronics
+# ======================================================================
 # ARM CMSIS 6
 FetchContent_Declare(
-    cmsis_core
+    arm_cmsis
     GIT_REPOSITORY https://github.com/ARM-software/CMSIS_6.git
     GIT_TAG v6.3.0
     GIT_SHALLOW TRUE
+    # Because CMSIS lib doesn't have a CMakeLists.txt, those two following keywords won't work, but just put here for future reference
+    SYSTEM
+    EXCLUDE_FROM_ALL
 )
-
-string(TOLOWER ${MCU_FAMILY} MCU_FAMILY_LOWER)
-string(SUBSTRING ${MCU_FAMILY_LOWER} 5 -1 FAMILY_SHORT) # e.g., extract "f1"
 
 # ST CMSIS Device F1
 FetchContent_Declare(
-    cmsis_device
-    GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis-device-${FAMILY_SHORT}.git
+    stm32_cmsis
+    GIT_REPOSITORY https://github.com/STMicroelectronics/cmsis-device-${MCU_FAMILY_SHORT}.git
     GIT_TAG v4.3.5
     GIT_SHALLOW TRUE
+    SYSTEM
+    EXCLUDE_FROM_ALL
 )
 
 # ST HAL Driver
@@ -25,6 +30,66 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/STMicroelectronics/${MCU_FAMILY_LOWER}xx-hal-driver.git
     GIT_TAG v1.1.10
     GIT_SHALLOW TRUE
+    SYSTEM
+    EXCLUDE_FROM_ALL
 )
 
-FetchContent_MakeAvailable(cmsis_core cmsis_device stm32_hal_driver)
+FetchContent_MakeAvailable(arm_cmsis stm32_cmsis stm32_hal_driver)
+
+
+# ======================================================================
+# Targets
+# ======================================================================
+# CMSIS Dependencies from both ARM and STMicroelectronics
+add_library(arm_cmsis INTERFACE)
+target_include_directories(arm_cmsis SYSTEM INTERFACE
+    ${arm_cmsis_SOURCE_DIR}/CMSIS/Core/Include
+)
+add_library(arm::cmsis ALIAS arm_cmsis)
+
+add_library(stm32_cmsis INTERFACE)
+target_include_directories(stm32_cmsis SYSTEM INTERFACE
+    ${stm32_cmsis_SOURCE_DIR}/Include
+)
+add_library(stm32::cmsis ALIAS stm32_cmsis)
+
+# LL Library: stm32::ll
+add_library(stm32_ll STATIC)
+target_link_libraries(stm32_ll PUBLIC stm32::cmsis arm::cmsis bsp::mcu_config)
+# Headers might couple with each other, so not use File Set to avoid missing headers
+target_include_directories(stm32_ll SYSTEM PUBLIC
+    ${stm32_hal_driver_SOURCE_DIR}/Inc
+    ${stm32_hal_driver_SOURCE_DIR}/Inc/Legacy
+)
+# Other consumers including those headers also need these macros
+target_compile_definitions(stm32_ll PUBLIC
+    ${MCU_MODEL}
+    USE_FULL_LL_DRIVER
+)
+target_sources(stm32_ll PRIVATE
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_ll_gpio.c
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_ll_rcc.c
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_ll_usart.c
+)
+add_library(stm32::ll ALIAS stm32_ll)
+
+# HAL Library: stm32::hal
+add_library(stm32_hal STATIC)
+target_link_libraries(stm32_hal PUBLIC stm32::cmsis arm::cmsis bsp::mcu_config)
+target_include_directories(stm32_hal SYSTEM PUBLIC
+    ${CMAKE_CURRENT_SOURCE_DIR}/bsp/hal_config
+    ${stm32_hal_driver_SOURCE_DIR}/Inc
+    ${stm32_hal_driver_SOURCE_DIR}/Inc/Legacy
+)
+target_compile_definitions(stm32_hal PUBLIC
+    ${MCU_MODEL}
+    USE_HAL_DRIVER
+)
+target_sources(stm32_hal PRIVATE
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_hal.c
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_hal_rcc.c
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_hal_gpio.c
+    ${stm32_hal_driver_SOURCE_DIR}/Src/${MCU_FAMILY_LOWER}xx_hal_cortex.c
+)
+add_library(stm32::hal ALIAS stm32_hal)
+
--- cmake/stm32f1.cmake
+++ cmake/stm32f1.cmake
@@ -1,34 +1,44 @@
-add_library(stm32_f1_flags INTERFACE)
-
+# ======================================================================
+# Flags for all static libraries and ELF
+# ======================================================================
+add_library(bsp_stm32f1_mcu_config INTERFACE)
 # Cortex-M3 core and FPU configs
 set(CPU_FLAGS
-    "-mcpu=cortex-m3"
-    "-mthumb"
-    "-mfloat-abi=soft"
+    -mcpu=cortex-m3
+    -mthumb
+    -mfloat-abi=soft
 )
 
 # Bare-metal common flags for optimization
-set(COMMON_FLAGS
+set(ARCH_OPT_FLAGS
     -Wall                           # Enable all warnings
     -Wextra                         # Enable extra warnings
     -fdata-sections                 # Prepare for GC optimization
     -ffunction-sections
 )
-target_compile_options(stm32_f1_flags INTERFACE
+target_compile_options(bsp_stm32f1_mcu_config INTERFACE
     ${CPU_FLAGS}
-    ${COMMON_FLAGS}
+    ${ARCH_OPT_FLAGS}
+)
+target_link_options(bsp_stm32f1_mcu_config INTERFACE
+    ${CPU_FLAGS}
 )
 
-# Linker flags
+add_library(bsp::mcu_config ALIAS bsp_stm32f1_mcu_config)
+
+# ======================================================================
+# Flags for final ELF target
+# ======================================================================
+add_library(stm32_f1_linker INTERFACE)
+target_link_libraries(stm32_f1_linker INTERFACE bsp::mcu_config)
 set(LINKER_FLAGS
     --specs=nano.specs
     -Wl,--gc-sections
     -Wl,--print-memory-usage
-    -Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map
+    "-Wl,-Map=$<TARGET_PROPERTY:NAME>.map"
 )
-target_link_options(stm32_f1_flags INTERFACE
-    ${CPU_FLAGS}
+target_link_options(stm32_f1_linker INTERFACE
     ${LINKER_FLAGS}
 )
 
-add_library(stm32::f1_flags ALIAS stm32_f1_flags)
+add_library(stm32::f1_linker ALIAS stm32_f1_linker)
