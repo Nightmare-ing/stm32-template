5d9fd739c7f8558bb0c68c4861ebac6222b10c81
Qingsong Hu <kingsanhu@gmail.com>
2026-09-11 17:02:32 +0800

    Add bin, hex and map file to clean target
    Also move target related link options to the target itself instead of the linker library.

--- cmake/stm32-utils.cmake
+++ cmake/stm32-utils.cmake
@@ -2,10 +2,12 @@
 # Automatically generate .hex and .bin files for STM32 targets after build
 # ======================================================================
 function(stm32_add_hex_bin_targets TARGET_NAME)
-    set(HEX_FILE "$<TARGET_FILE_DIR:${TARGET_NAME}>/$<TARGET_FILE_BASE_NAME:${TARGET_NAME}>.hex")
-    set(BIN_FILE "$<TARGET_FILE_DIR:${TARGET_NAME}>/$<TARGET_FILE_BASE_NAME:${TARGET_NAME}>.bin")
+    set(HEX_FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.hex")
+    set(BIN_FILE "${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.bin")
 
     add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+        BYPRODUCTS ${HEX_FILE} ${BIN_FILE}
+
         COMMAND ${CMAKE_OBJCOPY} -O ihex $<TARGET_FILE:${TARGET_NAME}> ${HEX_FILE}
         COMMAND ${CMAKE_OBJCOPY} -O binary $<TARGET_FILE:${TARGET_NAME}> ${BIN_FILE}
         COMMAND ${CMAKE_SIZE} --format=berkeley $<TARGET_FILE_NAME:${TARGET_NAME}>
